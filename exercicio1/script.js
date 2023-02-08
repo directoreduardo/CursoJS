c3909569e737d09651514742c46bf2c815d4951d
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-02-08 17:06:26 -0300

    !

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,16 +3,34 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if(fano.value.length==0||fano.value>ano){
+    if(fano.value.length==0||fano.value>ano){ //se não digitou o ano OU ENTÃO se o fano.value for maior que o ano atual:
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     }else{
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
+        var fsex = document.getElementsByName('radsex') //('redsex') [0] é a bolinha do masculino e o ('redsex') [1] é a bolinha do feminino
+        var idade = ano - Number(fano.value) //o valor atual menos(-) o valor digitado no fano 
         var genero = ''
         if(fsex[0].checked){
             genero = 'Homem'
-        }else{
+            if(idade>=0 && idade<10){
+                //criança
+            }else if(idade<21){
+                //jovem
+            }else if(idade<50){
+                //adulto
+            }else{
+                //idoso
+            }
+        }else{ // também pode ser: else if(fsex[1].checked){}
             genero = 'Mulher'
+            if(idade>=0 && idade<10){
+                //criança
+            }else if(idade<21){
+                //jovem
+            }else if(idade<50){
+                //adulto
+            }else{
+                //idoso
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
