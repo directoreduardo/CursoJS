1ddd7847a5e487cff615545ab39dec690b169224
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-02-09 13:55:32 -0300

    >

--- exercicio1.5/script.js
+++ exercicio1.5/script.js
@@ -1,3 +1,20 @@
 function contar(){
-    
+    //usando let ao invés de var
+    let ini = document.getElementById('txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.querySelector('div#res')
+
+    if(ini.value.length==0 || fim.value.length==0 || passo.value.length==0){ // se o inicio, fim e passo for igual a 0
+        alert('[ERRO] Faltam dados!')
+    }else{
+        res.isConnected = 'Contando: '
+        let i = Number(ini.value) // por mais que a caixa de texto seja numérica, ela não está como um número, por isso a conversão
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c=i; c<=f; c+=p){ // o contador vai começar no início e enquanto o contador for menor ou igual ao fim, o contador vai receber ele mesmo mais o passo
+
+        }
+    }
 }
