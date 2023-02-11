be297533a385c1f1122f0f43da2d2973fa46306b
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-02-11 14:30:22 -0300

    !

--- aulas/ex005.js
+++ aulas/ex005.js
@@ -8,15 +8,23 @@ function parimpar(n){
 }
 let res = parimpar(437)
 console.log(res)
-
+*/
 //--------------------
-
-function soma(n1=0, n2=0){
+/*
+function soma(n1=0, n2=0){ //=0 é um parametro pré definido caso, por algum motivo, eu não chame ou n1 ou o n2 - sem essa pré definição acabaria retornando NaN
     return n1 + n2
 }
 console.log(soma(5,9))
-
+*/
+//--------------------PASSAR PARA UMA VARIAVEL UM PARÂMETRO PARA EXECUTAR UMA FUNÇÃO:
+/*
+let v = function(x){
+    return x*2
+}
+console.log(v(5))
+*/
 //--------------------FATORIAL: TRADICIONAL:
+/*
 function fatorial(n){
     let fat = 1
     for(let c = n;c>1;c--){
@@ -25,8 +33,9 @@ function fatorial(n){
     return fat
 }
 console.log(fatorial(5))
-
+*/
 //-------------------FATORIAL: RECURSIVA:
+/*
 function fatorial(n){
     if (n==1){
         return 1
