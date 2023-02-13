a41294c65890076f6efbe0c041a37bb45804749a
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-02-12 21:08:50 -0300

    !

--- aulas/ex002.js
+++ aulas/ex002.js
@@ -1,3 +1,28 @@
+// OPERADORES TERNÁRIOS (?:) - EXEMPLO:
+let idade = 17
+idade >= 18? console.log('Maior de idade'): console.log('Menor de idade')
+// Outro exemplo, com funções, utilizando operadores ternários e if/else em casos não muito complexos:
+//--- com if/else:
+/*
+let somaOuMulti = function (valor){
+    if(valor>=10){
+        return valor+valor
+    }else{
+        return valor*valor
+    }
+}
+console.log(somaOuMulti(11))*/
+//--- A mesma formula, só que usando operadores ternários:
+let somaOuMulti = function (valor){
+    return valor>=10?valor+valor:valor*valor
+}
+console.log(somaOuMulti(11))
+// + arrow function:
+/*
+let somaOuMulti = (valor) => valor>=10?valor+valor:valor*valor
+console.log(somaOuMulti(11))*/
+
+// SWITCH:
 var agora = new Date()
 var diaSem = agora.getDay()
 console.log(diaSem)
@@ -24,3 +49,17 @@ switch(diaSem){ //switch é uma expressão [switch(expressão)], não um condiç
         console.log('Sabado')
         break  
 } // "default:" seria o equivalente ao "else{}" do 'if(){}'. Porém é opcional, assim como o "break" na ultima sentença
+// outro exemplo:
+/*
+let exemplo = 2
+switch(exemplo){
+    case 1:
+        console.log('exemplo 1')
+        break
+    case 2:
+        console.log('exemplo 2')
+        break
+    case 3:
+        console.log('exemplo 3')        
+} // retornará: 'exemplo 2'
+*/
