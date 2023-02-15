ac8d86db0d6c8730f6e63feee418e94e37cda53b
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-02-15 15:32:37 -0300

    !

--- aulas/ex006.object.js
+++ aulas/ex006.object.js
@@ -1,5 +1,44 @@
-// exemplos de objetos
+// Um objeto é uma coleção de dados relacionados e/ou funcionalidades (que geralmente consitem em diversas variáveis e funções - que são chamadas propriedades e métodos quando estão dentro de um objeto)
+// Objetos podem ser inicializados usando new Object(), Object.create() ou usando (a mais usada) uma forma notação literal
 
+// Usando a notação literal:
+/*
+let objetoVazio = {
+    prop1: // — propriedades são estáticas: podendo aqui ser um número, string, array. Porém também pode ser uma função (se tornando um método)
+}
+*/
+// Declarando
+let dotNotation = {
+    prop1: 'Sou uma propriedade que será acessada usando dot Notation', // não esquecer das virgulas!
+    metodo: function(){
+        return 'Sou o retorno de um método, ou seja, uma função dentro de um objeto'
+    }
+}
+// Acessando a propriedade e o método:
+console.log(dotNotation.prop1)
+console.log(dotNotation.metodo) // uma referencia para essa função
+console.log(dotNotation.metodo()) // chamando a função
+// Declarando uma nova propriedade e depois um novo método:
+console.log('=-=-=-=-=-=')
+dotNotation.prop2 = 'Sou outra propriedade com valor tipo String'
+dotNotation.metodo2 = () => 'Sou o retorno de outro método' // usando arrow function
+// Acesando os novos componentes:
+console.log(dotNotation.prop2)
+console.log(dotNotation.metodo2())
+
+// Usando Bracket notation (notação de colchetes) - é uma outra forma de acessar componentes de um método:
+/*
+let bracketNotation = {}
+console.log(bracketNotation['prop1'])
+console.log(bracketNotation['metodo']()) 
+// Também pode declarar novas propriedades e métodos - unica diferença é trocar o .prop1 por ['prop1]
+*/
+
+
+
+
+//CeV exemplo
+console.log('------------------------------------------------------')
 let amigo ={
     nome: 'Jose',
     sexo: 'M',
