06861c3fc03aaeabc72f798ae862b05e47179c99
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-02-20 21:24:29 -0300

    !

--- aulas/ex010.this.js
+++ aulas/ex010.this.js
@@ -10,6 +10,7 @@ console.log(window.a) // - 37
 /*
 // Quando não estiver no modo estrito o this apontará para o objeto global:
 function thisNaoEstrito(){console.log(this)}
+
 // Em modo estrito, o valor de this permanece seja qual for o definido ao entrar no contexto de execução, assim, no caso a seguir, this por padrão será indefinido (undufined)
 function thisEstrito(){'use strict'; console.log(this)}
 */
@@ -22,10 +23,11 @@ let o = {
     }
 }
 console.log(o.f())
+console.log('-------------------------------')
 
 // Função Arrow 
 // Nas arrow  function, o this é definido lexicalmnte, isto é, seu valor é definido pelo contexto de execução onde está inserido. Em um código global, this assume o objeto global:
-
+/*
 // CONTEXTO DE CRIAÇÃO: GLOBAL
 let arrowThis = () => {console.log(this)}
 
@@ -42,3 +44,17 @@ let obj2 = {
         console.log((() => this)())
     }
 }
+*/
+//----------------BIND---
+// O método bind() cria uma nova função que, quando chamada, tem sua palavra-chave this definida com o valor fornecido, com uma sequência determinada de argumentos precedendo quaisquer outros que sejam fornecidos quando a nova função é chamada
+
+//Exemplo
+//'use strict'
+function thisBindEx(){
+    console.log(this) // sem o 'use strict' o this aponta para o objeto global
+}
+// ligando a função thisBindEx() a um objeto:
+let obj = {exemplo: 'exemplo'}
+thisBindEx = thisBindEx.bind(obj) // ao fazer essa atribuição, o this da função thisBindEx() não será mais global, mas o objeto 'obj'
+thisBindEx()
+console.log('-------------------------------')
