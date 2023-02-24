15ad5cdf3f90a861a0271be22f5343fe2561c0b8
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-02-24 16:00:43 -0300

    ?

---
+++ aulas/ex013.String.2.js
@@ -0,0 +1,4 @@
+// MÉTODOS DE AÇÕES
+let stringTransformada // undefined
+let umaString = 'Essa é uma string de teste de estudos dos Métodos de String'
+console.log(stringTransformada = umaString.repeat(3)) // cria copia da string principal, a string na qual voce está chamando o método
