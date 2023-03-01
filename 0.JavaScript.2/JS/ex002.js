d8fa7959fc74d280ff2b7926d54eaaf0de3ef8e3
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-03-01 19:38:52 -0300

    >

--- 0.JavaScript.2/JS/ex002.js
+++ 0.JavaScript.2/JS/ex002.js
@@ -1,4 +1,4 @@
-// --------Herança
+// ---------------------HERANÇA
 
 class Dev{
     constructor(nome, idade, Linguagem, tipoDev){
@@ -14,23 +14,25 @@ class Dev{
 
 // especializando para um Frontends e Backends:
 class FrontendDev extends Dev{
-    constructor(nome, idade, Linguagem, tipoDev, framework){
-        super(nome, idade, Linguagem, tipoDev)
+    constructor(nome, idade, Linguagem, framework){
+        super(nome, idade, Linguagem)
         this.framework = framework
+        this.tipoDev = 'Frontend'
     }
 }
-let frontend1 = new FrontendDev('Pedro', 35, 'JavaScript', 'Frontend', 'React')
+let frontend1 = new FrontendDev('Pedro', 35, 'JavaScript', 'React')
 console.log(frontend1)
 frontend1.saudacao()
 console.log('----------')
 
 class BackendDev extends Dev{
-    constructor(nome, idade, Linguagem, tipoDev, bancoDeDados){
-        super(nome, idade, Linguagem, tipoDev)
+    constructor(nome, idade, Linguagem, bancoDeDados){
+        super(nome, idade, Linguagem)
         this.bancoDeDados = bancoDeDados
+        this.tipoDev = 'Backend'
     }
 }
-let backend1 = new BackendDev('João', 25, "C#", 'Backend', 'SQL Server')
+let backend1 = new BackendDev('João', 25, "C#", 'SQL Server')
 console.log(backend1)
 backend1.saudacao()
 console.log('--=-=---=-=-=--==-=-=-=-=-=-=-=--=-=-=-=-=--')
@@ -61,3 +63,37 @@ let gatoRaivoso = {
 }
 Object.setPrototypeOf(gatoRaivoso, gato)
 gatoRaivoso.miarForte()
+console.log('--=-=---=-=-=--==-=-=-=-=-=-=-=--=-=-=-=-=--')
+console.log()
+
+// ---------------------HERANÇA SEM USAR CLASSES
+// Usando de exemplo o código acima (HERANÇA): "transportando" para o JS:
+
+let Dev2 = function(nome, idade, Linguagem, tipoDev){
+    this.nome = nome
+    this.idade = idade
+    this.Linguagem = Linguagem
+    this.tipoDev = tipoDev
+    this.saudacao = function(){
+        console.log(`Oi! Sou dev ${this.tipoDev} tenho ${this.idade} anos, trabalho com ${this.Linguagem} e me chamo ${this.nome}`)
+    }
+}
+
+let FrontendDev2 = function(nome, idade, Linguagem, framework){
+    let newDev2 = new Dev2(nome, idade, Linguagem, 'Frontend')
+    newDev2.framework = framework
+    Object.setPrototypeOf(this, newDev2)  // o this de FrontendDev2 vai ser prototype de newDev2
+}
+let frontend2 = new FrontendDev2('Pedro(2)', 35, 'JavaScript(2)', 'React')
+console.log(frontend2)
+frontend2.saudacao()
+console.log('----------')
+
+let BackendDev2 = function(nome, idade, Linguagem, bancoDeDados){
+    let newDev2 = new Dev2(nome, idade, Linguagem, 'Backend')
+    newDev2.bancoDeDados = bancoDeDados
+    Object.setPrototypeOf(this, newDev2)
+}
+let backend2 = new BackendDev2('João(2)', 25, "C#(2)", 'SQL Server')
+console.log(backend2)
+backend2.saudacao()
