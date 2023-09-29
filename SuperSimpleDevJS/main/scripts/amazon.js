031064e9dcef173cbf855316b57f82226ba92093
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-09-28 21:59:00 -0300

    A

---
+++ SuperSimpleDevJS/main/scripts/amazon.js
@@ -0,0 +1,34 @@
+// 1. "Save the data" - Vamos mover os dados em html e salvar em javascript
+
+// Primeiro uma variável para salvar os dados, em seguida vamos criar algo que corresponda aos nossos dados. Neste caso, temos uma lista de produtos, portanto vamos criar um array porque um array representa uma lista. Os produtos da lista tem muitos valores, como imagem, nome e preço, então usaremos um objeto para representar cada produto, porque um objeto nos permite agrupar vários valores.
+
+let products = [{
+  image: 'imagens/products/athletic-cotton-socks-6-pairs.jpg" class="product-image',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  // como JS tem problemas para fazer contas com números decimais ou flutuantes (como 10.90). portanto uma prática recomendada ao calcular o dinheiro é calcular em centavos em vez de dólares
+  priceCents: 1090
+}, /*segundo produto neste objeto*/ {
+  image: 'imagens/products/intermediate-composite-basketball.jpg" class="product-image',
+  name: 'Intermediate Size Basketball',
+  rating: {
+    stars: 4,
+    count: 127
+  },
+  priceCents: 2095
+}, {
+  image: 'imagens/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" class="product-image',
+  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 799
+}]
+
+// 2. "Generate the HTML" - Agora vamos usar esses dados para gerar o HTML, em vez de escrever o HTML manualmente
+
+// Para gerá-lo, podemos criar um loop no array. Para cada um desses produtos (> dentro do objeto > dentro do array), vamos criar algum HTML
