56632f95854fe8632fdac3151dba56879f052fc7
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-10-14 18:01:36 -0300

    a

---
+++ 1.javascript-Masterry/segunda etapa/scripts/script.js
@@ -0,0 +1,20 @@
+let myImage = ['image/image1.jpg', 'image/image2.jpg', 'image/image3.jpg', 'image/image4.jpg', 'image/image5.jpg']
+let currentImage = 0 // quando o slideshow carregar, é a primeira imagem estará que será mostrada, no array, este elemento é o numero 0
+//------NEXT----------
+function nextPhoto() {
+  currentImage++
+  if (currentImage > myImage.length-1) {
+    currentImage = 0
+  }
+  document.querySelector('.myimage').src = myImage[currentImage]
+}
+let nextBtn = document.querySelector('.next').onclick = nextPhoto
+//------PREVIOUS-------
+function previousPhoto() {
+  currentImage-- 
+  if (currentImage < 0) {
+    currentImage = myImage.length-1
+  }
+  document.querySelector('.myimage').src = myImage[currentImage]
+}
+document.querySelector('.previous').onclick = previousPhoto
