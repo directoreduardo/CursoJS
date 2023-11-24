23a879a499ca05b8e01eef6b49f9c45de49c06b7
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2023-11-23 21:58:48 -0300

    a

--- 1.javascript-Masterry/12 jQuery Plugins/Basic FlexSlider/myscript.js
+++ 1.javascript-Masterry/12 jQuery Plugins/Basic FlexSlider/myscript.js
@@ -1,3 +1,12 @@
-$(window).load(function() {
-  $(".flexslider").flexslider() // esta função apenas existe porque nos temos o plugin, o plugin é a definição desta função. É uma adição do jQuery
+$(window).on('load',function() { //por que não apenas "load(function(){})"? - porque, caso a versão do jQuery atualize, por exemplo, essa que estou usando é a 3.7.1, provavelmente daria erro e só aceitaria a versão anterior. Usando esse metodo acima, acaba sendo funcional para versões mais atuais
+  $(".flexslider").flexslider({
+    //- adicionando anmiações customizadas (todos os exemplos estão no site: woo.com/flexslider/). Exemplos:
+
+    /* animation: 'slide',
+    direction: 'vertical',
+    reverse: true, */
+    slideshowSpeed: 3000,
+    pauseOnHover: true // basicamente acelerei a 'mostragem de slide automatica' pra cada 3 segundos, e adicionei um 'pausar a mostragem' ao mover o mouse acima do slide
+    
+  }) // esta função (flexslider()) apenas existe porque nos temos o plugin, o plugin é a definição desta função. É uma adição do jQuery
 })
