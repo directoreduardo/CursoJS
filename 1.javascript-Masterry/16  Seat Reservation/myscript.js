2c0b8a4326e8cd4ba1310cdb7fbf47a049db9d1d
Eduardo Henrique Padilha <112039658+directoreduardo@users.noreply.github.com>
2024-01-10 18:55:44 -0300

    A

--- 1.javascript-Masterry/16  Seat Reservation/myscript.js
+++ 1.javascript-Masterry/16  Seat Reservation/myscript.js
@@ -1,8 +1,54 @@
+/* ----------------------------------------- */
+/* MANEIRA "ROMOTA" E ULTRAPASSADO DE FAZER: */
+/* const rows = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t"] */
 
+/* --------THE LEFT SIDE-------- */
+/* let html = ""
+let counter = 1
 
+// o método forEach() é um método array, o que significa que você o executa em um array, como "linhas" ("rows"). Então você pode passar uma função, e essa função recebe um argumento que representa cada elemento do array.
+rows.forEach(function(row) {
+  html += `<div class="label">${row}</div>`
 
-// Para cada coluna...
+  for(let i=0; i<3; i++) {
+    html += `<div id="${row + counter}">${counter}</div>`
+    counter++
+  }
+  //isso com certeza está gerando os assentos, mas a numeração está errada. A fila "B" começa no assento 4, mas você realmente quer que comece no assento 16 
+  counter = counter + 12
+})
+document.querySelector('#left').innerHTML = html */
+/* ------THE RIGHT SIDE------- */
+/* html = ""
+counter = 1
+rows.forEach(function(row) {
+  counter = counter + 12 //o contador +12 é adicionado acima
+  for(let i=0; i<3; i++) {
+    html += `<div id="${row + counter}">${counter}</div>`
+    counter++
+  }
+  //e o label fica por ultimo 
+  html += `<div class="label">${row}</div>`
+})
+document.querySelector('#right').innerHTML = html */
+/* -------MIDDLE SECTION------- */
+/* html = ""
+counter = 1
+rows.forEach(function(row) {
+  counter = counter + 3 //o contador agora é +3
+  for(let i=0; i<9; i++) {// muda do 3 pro 9 
+    html += `<div id="${row + counter}">${counter}</div>`
+    counter++
+  }
+  //repete o contador acima 
+  counter = counter + 3
+  //não precisamos do label aqui 
+})
+document.querySelector('#middle').innerHTML = html */
 
+/* ----------------------------------------- */
+/* ----MODO MAIS RESUMIDO É "MODERNO----" */
+/* // Para cada coluna... */
   // Se eu estou na seção direita, esquerda ou no centro, faça a coisa certa...
   // Percorra (loop) os assentos dessa seção
   
@@ -11,19 +57,18 @@ function makeRows(sectionLength, rowLength, placement) {
   let html = ""
   let counter = 1
 
-  rows.forEach(function(row) {
+  rows.forEach(row => {
     switch(placement) {
       /* fazer um rótulo (label) */
-      case "left": html += `<div class="label">${row}</div>`
+      case "left": html += `<div class="label">${row}</div>`;
       break
 
       /* adicionar 12 ao contador */
-      case "right": counter = counter + (rowLength - sectionLength) /*pq o rowLength(comprimento da coluna) é 15, e o sectionLength(comprimento da seção) é 3. O resultado: 12*/
+      case "right": counter = counter + (rowLength - sectionLength); /*pq o rowLength(comprimento da coluna) é 15, e o sectionLength(comprimento da seção) é 3. O resultado: 12*/
       break      
 
       /* (centro) adicionar 3 ao contador */
-      default: counter = counter + ((rowLength - sectionLength)/2)
-      break
+      default: counter = counter + ((rowLength - sectionLength)/2);
     }
 
     //loop aqui (adcionando uma classe)
@@ -34,16 +79,15 @@ function makeRows(sectionLength, rowLength, placement) {
 
     switch(placement) {
       //adicionar 12 ao contador
-      case "left": counter = counter + (rowLength - sectionLength)
+      case "left": counter = counter + (rowLength - sectionLength);
       break
 
       //fazer um rótulo (label)
-      case "right": html += `<div class="label">${row}</div>`
+      case "right": html += `<div class="label">${row}</div>`;
       break
 
       //(centro) adicionar 3 ao contador
-      default: counter = counter + ((rowLength - sectionLength)/2)
-      break
+      default: counter = counter + ((rowLength - sectionLength)/2);
     }
   })
   document.getElementById(placement).innerHTML = html
@@ -54,4 +98,4 @@ function makeRows(sectionLength, rowLength, placement) {
 /* execute esta função três vezes - com parâmetros definidos para cada seção - e você terá todos os 300 assentos exibidos em sua página. */
 makeRows(3, 15, 'left')
 makeRows(3, 15, 'right')
-makeRows(3, 15, 'middle')
+makeRows(9, 15, 'middle')
